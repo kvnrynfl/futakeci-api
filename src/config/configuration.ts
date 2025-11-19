a10c15becbc0e4feb6016284961f73e1dad9da86
kvnrynfl <kevinreynaufal2004@gmail.com>
2025-11-19 13:12:48 +0700

    feat: add application configuration and environment example

---
+++ src/config/configuration.ts
@@ -0,0 +1,6 @@
+export default () => ({
+  DATABASE_URL: process.env.DATABASE_URL,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
+  PORT: process.env.PORT || 3000,
+});
